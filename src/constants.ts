0db98d475308e638fd1b2a2154c6730e217d17b9
David Kerr <kerrky@gmail.com>
2024-10-03 19:01:35 -0400

    Created pagination functionality

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ export const SITE_TITLE = "GearHeadz Blog";
 export const SITE_DESCRIPTION =
   "Articles, stories and tutorials from Gear Heads";
 export const HOMEPAGE_ARTICLE_LIMIT = 3;
-export const ARTICLES_PER_PAGE = 6;
+export const ARTICLES_PER_PAGE = 4;
